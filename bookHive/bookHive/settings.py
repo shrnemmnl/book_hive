09cf8ffa579b74e0c0d9d12b8c9ef7cde22c7bc2
shrnemmnl <shrnemmnl@gmail.com>
2025-08-07 18:33:48 +0530

    cnfiguring email

--- bookHive/bookHive/settings.py
+++ bookHive/bookHive/settings.py
@@ -171,8 +171,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'  # use smtp.mailtrap.io or any service you like
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'shrnemmnl@gmail.com'
-EMAIL_HOST_PASSWORD = 'vykb qmzd tmtj nmkw'  # not your Gmail password!
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')  # not your Gmail password!
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
